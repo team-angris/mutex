80538b3e9165e5e57888bb60ca926125c7f92fe5
tharunoptimus <tharunoptimus@outlook.com>
2022-12-07 14:46:52 +0530

    Added data for technical events

---
+++ pages/Events/data.js
@@ -0,0 +1,32 @@
+export default function Home() {
+	return <></>
+}
+
+let dataset = [
+	{
+		heading: "Technical",
+		numberOfEvents: "10",
+		description:
+			"If you are a dreamer with a keen eye for detail, specializing in stunning visual imagery. If you are imaginative and can translate communication into design, then connect with us by sending in your application.",
+		hyperlink: "/events/technical",
+		bgImageURL: './events/technical.webp',
+	},
+	{
+		heading: "Non Technical",
+		numberOfEvents: "8",
+		description:
+			"If you are a dreamer with a keen eye for detail, specializing in stunning visual imagery. If you are imaginative and can translate communication into design, then connect with us by sending in your application.",
+		hyperlink: "/events/technical",
+		bgImageURL: './events/nontechnical.webp',
+	},
+	{
+		heading: "Technical",
+		numberOfEvents: "8",
+		description:
+			"If you are a dreamer with a keen eye for detail, specializing in stunning visual imagery. If you are imaginative and can translate communication into design, then connect with us by sending in your application.",
+		hyperlink: "/events/technical",
+		bgImageURL: './events/nontechnical.webp',
+	},
+]
+
+export { dataset }
