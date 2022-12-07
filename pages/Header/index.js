756ddf28c26b2f0e161d1d41aa871b1b474e0f21
tharunoptimus <tharunoptimus@outlook.com>
2022-12-07 16:39:33 +0530

    Added `#` navigation with `id`

--- pages/Header/index.js
+++ pages/Header/index.js
@@ -2,23 +2,27 @@ import styles from "./header.module.css"
 
 export default function Header() {
 	return (
-		<div className={styles.container}>
+		<div className={styles.container} id="header">
 			<div className={styles.flexOne}></div>
 			
-			<NavOption tagName="Home" />
-			<NavOption tagName="About" />
-			<NavOption tagName="Events" />
-			<NavOption tagName="Sponsors" />
-			<NavOption tagName="Contacts" />
+			<NavOption tagName="Home" hyperlink="home"/>
+			<NavOption tagName="About" hyperlink="about"/>
+			<NavOption tagName="Events" hyperlink="events"/>
+			<NavOption tagName="Sponsors" hyperlink="sponsors"/>
+			<NavOption tagName="Contacts" hyperlink="contact"/>
 		</div>
 	)
 }
 
 export function NavOption(props) {
-	return (
 
+	let handleClick = () => {
+		window.location = `#${props.hyperlink}`
+	}
+
+	return (
 		<div className={styles.navOption}>
-			<span>{props.tagName}</span>
+			<span className={styles.navSpan} onClick={handleClick}>{props.tagName}</span>
 		</div>
 
 	)
