b15a22f354cdbe733444a0573809f78fc8cda856
tharunoptimus <tharunoptimus@outlook.com>
2022-12-07 15:50:09 +0530

    Added sponsors

---
+++ pages/Sponsors/index.js
@@ -0,0 +1,13 @@
+import styles from "./sponsors.module.css"
+
+export default function Home() {
+	return (
+		<div className={styles.sponsors}>
+			<span className={styles.heading}>Sponsors</span>
+
+			<div className={styles.sponsorsContainer}>
+				
+			</div>
+		</div>
+	)
+}
