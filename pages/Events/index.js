5f95d29e02432168a211d258af5b3c5d622ea5b3
tharunoptimus <tharunoptimus@outlook.com>
2022-12-07 14:47:03 +0530

    Added `Events`

---
+++ pages/Events/index.js
@@ -0,0 +1,50 @@
+import { v4 as uuidv4 } from "uuid"
+import styles from "./events.module.css"
+import { dataset } from "./data"
+
+export default function Home() {
+	return (
+		<div className={styles.events}>
+			<span className={styles.heading}>Events</span>
+
+			<div className={styles.eventsContainer}>
+				{dataset.map((item) => {
+					let id = uuidv4()
+					return (
+						<EventCard
+							key={id}
+							heading={item.heading}
+							numberOfEvents={item.numberOfEvents}
+							description={item.description}
+							hyperlink={item.hyperlink}
+							bgImageURL={item.bgImageURL}
+						/>
+					)
+				})}
+			</div>
+
+			<div className={styles.designThing}></div>
+		</div>
+	)
+}
+
+function EventCard(props) {
+	// props would contain heading, numberOfEvents, description, hyperlink, bgImageURL
+
+	return (
+		<div
+			className={styles.eventCard}
+			style={{ backgroundImage: `url(${props.bgImageURL})` }}
+		>
+			<span className={styles.eventHeading}>{props.heading}</span>
+			<span className={styles.eventHeading}>Events</span>
+			<span className={styles.numberOfEvents}>
+				{props.numberOfEvents}{"+ EVENTS"}
+			</span>
+			<span className={styles.description}>{props.description}</span>
+			<a href={props.hyperlink} className={styles.hyperlink}>
+				<span>Explore</span>
+			</a>
+		</div>
+	)
+}
