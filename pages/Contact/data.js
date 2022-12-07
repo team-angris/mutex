f3e86320f1868dd5545e6ccd3d9528662811257e
tharunoptimus <tharunoptimus@outlook.com>
2022-12-07 16:32:11 +0530

    Added `Contact`

---
+++ pages/Contact/data.js
@@ -0,0 +1,46 @@
+export default function Data() {
+    return (
+        <></>
+    )
+}
+
+
+let technical = [
+	{
+		personName: "Person 1",
+		email: "Lorem Ipsum",
+	},
+	{
+		personName: "Person 2",
+		email: "Lorem Ipsum",
+	},
+	{
+		personName: "Person 3",
+		email: "Lorem Ipsum",
+	},
+	{
+		personName: "Person 4",
+		email: "Lorem Ipsum",
+	}
+]
+
+let nonTechnical = [
+	{
+		personName: "Person 1",
+		email: "Lorem Ipsum",
+	},
+	{
+		personName: "Person 2",
+		email: "Lorem Ipsum",
+	},
+	{
+		personName: "Person 3",
+		email: "Lorem Ipsum",
+	},
+	{
+		personName: "Person 4",
+		email: "Lorem Ipsum",
+	}
+]
+
+export { technical, nonTechnical }
---
+++ pages/Contact/index.js
@@ -0,0 +1,64 @@
+import styles from "./contact.module.css"
+import { v4 as UUID4 } from "uuid"
+import { technical, nonTechnical } from "./data"
+
+export default function Home() {
+	return (
+		<div className={styles.contact}>
+			<span className={styles.heading}>Contact</span>
+			<div className={styles.contactContainer}>
+				<Technical technical={technical} />
+				<NonTechnical nonTechnical={nonTechnical} />
+			</div>
+		</div>
+	)
+}
+
+function Person(props) {
+	return (
+		<div className={styles.personItem}>
+			<span className={styles.personItemName}>{props.personName}</span>
+			<span className={styles.personItemEmail}>{props.email}</span>
+		</div>
+	)
+}
+
+function Technical(props) {
+	return (
+		<div className={styles.contactItem}>
+			<span className={styles.contactItemHeading}>Technical Events</span>
+			<div className={styles.contactItemContent}>
+				{props.technical.map((item) => {
+					return (
+						<Person 
+							key={UUID4()}
+							personName={item.personName}
+							email={item.email}
+						/>
+					)
+				}
+				)}
+			</div>
+		</div>
+	)
+}
+
+function NonTechnical(props) {
+	return (
+		<div className={styles.contactItem}>
+			<span className={styles.contactItemHeading}>Non Technical Events</span>
+			<div className={styles.contactItemContent}>
+				{props.nonTechnical.map((item) => {
+					return (
+						<Person 
+							key={UUID4()}
+							personName={item.personName}
+							email={item.email}
+						/>
+					)
+				}
+				)}
+			</div>
+		</div>
+	)
+}
