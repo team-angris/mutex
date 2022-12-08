8603fe7233610d92171945ac69c21f34dee191d6
tharunoptimus <tharunoptimus@outlook.com>
2022-12-09 03:10:15 +0530

    Added images to about

--- pages/Components/About/index.js
+++ pages/Components/About/index.js
@@ -30,8 +30,8 @@ export default function Home() {
 }
 
 const Images = () => {
-	const images = [9, 8, 7, 6, 5].map((number) => ({
-		src: `https://placedog.net/${number}00/${number}00?id=${number}`,
+	const images = [1, 3, 4, 5].map((number) => ({
+		src: `./posters/about/${number}.webp`,
 	}))
 
 	return (
@@ -42,6 +42,7 @@ const Images = () => {
 			hasIndexBoard={false}
 			hasLeftButton={"bottomLeft"}
 			hasRightButton={"bottomRight"}
+			hasThumbnails={false}
 		/>
 	)
 }
