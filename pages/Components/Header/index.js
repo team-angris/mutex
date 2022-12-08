dc4d78cecca4e7c62d28459bbacc32cc1f8fbdec
tharunoptimus <tharunoptimus@outlook.com>
2022-12-09 03:18:24 +0530

    Added Next `Link` for seamless nav

--- pages/Components/Header/index.js
+++ pages/Components/Header/index.js
@@ -1,29 +1,26 @@
 import styles from "./header.module.css"
+import Link from "next/link"
 
 export default function Header() {
 	return (
 		<div className={styles.container} id="header">
 			<div className={styles.flexOne}></div>
-			
-			<NavOption tagName="Home" hyperlink="home"/>
-			<NavOption tagName="About" hyperlink="about"/>
-			<NavOption tagName="Events" hyperlink="events"/>
-			<NavOption tagName="Sponsors" hyperlink="sponsors"/>
-			<NavOption tagName="Contacts" hyperlink="contact"/>
+
+			<NavOption tagName="Home" hyperlink="home" />
+			<NavOption tagName="About" hyperlink="about" />
+			<NavOption tagName="Events" hyperlink="events" />
+			<NavOption tagName="Sponsors" hyperlink="sponsors" />
+			<NavOption tagName="Contacts" hyperlink="contact" />
 		</div>
 	)
 }
 
 export function NavOption(props) {
-
-	let handleClick = () => {
-		window.location = `#${props.hyperlink}`
-	}
-
 	return (
 		<div className={styles.navOption}>
-			<span className={styles.navSpan} onClick={handleClick}>{props.tagName}</span>
+			<Link href={`/#${props.hyperlink}`}>
+				<span className={styles.navLink}>{props.tagName}</span>
+			</Link>
 		</div>
-
 	)
 }
