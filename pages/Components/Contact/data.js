157e24662479b1cdeaf268e43fce2c25de46e3e6
tharunoptimus <tharunoptimus@outlook.com>
2022-12-09 03:59:57 +0530

    Added contact numbers

--- pages/Components/Contact/data.js
+++ pages/Components/Contact/data.js
@@ -7,39 +7,163 @@ export default function Data() {
 
 let technical = [
 	{
-		personName: "Person 1",
-		email: "Lorem Ipsum",
+		personName: "Meyyappan",
+		email: "9080033696",
 	},
 	{
-		personName: "Person 2",
-		email: "Lorem Ipsum",
+		personName: "Manoj",
+		email: "8248555492",
 	},
 	{
-		personName: "Person 3",
-		email: "Lorem Ipsum",
+		personName: "Madhav Krishnaa",
+		email: "7338964197",
 	},
 	{
-		personName: "Person 4",
-		email: "Lorem Ipsum",
-	}
+		personName: "Aakash",
+		email: "9841738989",
+	},
+	{
+		personName: "Shrinitha",
+		email: "9003290095",
+	},
+	{
+		personName: "Sai Anchana",
+		email: "8939825128",
+	},
+	{
+		personName: "Balasubramanian",
+		email: "9444876751",
+	},
+	{
+		personName: "Aadhitya",
+		email: "9884113766",
+	},
+	{
+		personName: "Kailash",
+		email: "9095541133",
+	},
+	{
+		personName: "Vignesh",
+		email: "8825708044",
+	},
+	{
+		personName: "Krishnan",
+		email: "6383551677",
+	},
+	{
+		personName: "Sneha Pillai",
+		email: "7358733362",
+	},
+	{
+		personName: "Aarthi Iyer",
+		email: "unavailable",
+	},
+	{
+		personName: "Aditya Kumar",
+		email: "9600194782",
+	},
+	{
+		personName: "Yashwanth",
+		email: "7010103495",
+	},
+	{
+		personName: "Vasudevakrishna",
+		email: "6383943553",
+	},
+	{
+		personName: "Thrishya",
+		email: "9384011184",
+	},
+	{
+		personName: "Sudharsana",
+		email: "9513787803",
+	},
+	{
+		personName: "Abhijit",
+		email: "9840406710",
+	},
+	{
+		personName: "Mohamed Fazith",
+		email: "9080527253",
+	},
+	{
+		personName: "Rozen Berg",
+		email: "unavailable",
+	},
+	{
+		personName: "Ezhil Mukhi",
+		email: "7305774686",
+	},
+	{
+		personName: "Monika",
+		email: "9840867891",
+	},
 ]
 
 let nonTechnical = [
 	{
-		personName: "Person 1",
-		email: "Lorem Ipsum",
+		personName: "Srinath",
+		email: "9789649388",
 	},
 	{
-		personName: "Person 2",
-		email: "Lorem Ipsum",
+		personName: "Shreeja",
+		email: "9150445617",
 	},
 	{
-		personName: "Person 3",
-		email: "Lorem Ipsum",
+		personName: "Logeshwaran",
+		email: "7339122647",
 	},
 	{
-		personName: "Person 4",
-		email: "Lorem Ipsum",
+		personName: "Riyaz Ahmed",
+		email: "9150567713",
+	},
+	{
+		personName: "Aravinth kumar",
+		email: "8870335281",
+	},
+	{
+		personName: "Vishnu",
+		email: "8939430854",
+	},
+	{
+		personName: "Sanjay Kannan",
+		email: "8428929784",
+	},
+	{
+		personName: "Srevardhani",
+		email: "9360120793",
+	},
+	{
+		personName: "Dafni Trisha",
+		email: "9443192988",
+	},
+	{
+		personName: "Geethanjali",
+		email: "6383051844",
+	},
+	{
+		personName: "Pooja",
+		email: "7395915055",
+	},
+	{
+		personName: "Gnanavarshini",
+		email: "6380614424",
+	},
+	{
+		personName: "Sowmya",
+		email: "8056493106",
+	},
+	{
+		personName: "Surya",
+		email: "9150986251",
+	},
+	{
+		personName: "Jagan",
+		email: "7540088216",
+	},
+	{
+		personName: "Veejendhiran P",
+		email: "6383647169",
 	}
 ]
 
