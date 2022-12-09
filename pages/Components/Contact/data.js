d1b9621201142361c375ea7f26ebe923d676eb20
tharunoptimus <tharunoptimus@outlook.com>
2022-12-09 15:39:54 +0530

    Modified Contact Details

--- pages/Components/Contact/data.js
+++ pages/Components/Contact/data.js
@@ -4,167 +4,47 @@ export default function Data() {
     )
 }
 
-
-let technical = [
+let members = [
 	{
-		personName: "Meyyappan",
-		email: "9080033696",
+		personName: "Yaswanth R",
+		phone: "63828 99284",
+		post: "President",
 	},
 	{
-		personName: "Manoj",
-		email: "8248555492",
+		personName: "Sanjay Kannan M",
+		phone: "84289 29784",
+		post: "General Secretary",
 	},
 	{
-		personName: "Madhav Krishnaa",
-		email: "7338964197",
+		personName: "Mohamed Fazith J",
+		phone: "90805 27253",
+		post: "Technical Head",
 	},
 	{
-		personName: "Aakash",
-		email: "9841738989",
+		personName: "Rozen Berg D",
+		phone: " 63828 07597",
+		post: "Media Head",
 	},
 	{
-		personName: "Shrinitha",
-		email: "9003290095",
+		personName: "Sudharsana Suresh",
+		phone: "95137 87803",
+		post: "Lady Representative",
 	},
 	{
-		personName: "Sai Anchana",
-		email: "8939825128",
+		personName: "Praveein S",
+		phone: "74026 85740",
+		post: "Joint Secretary",
 	},
 	{
-		personName: "Balasubramanian",
-		email: "9444876751",
+		personName: "Edwin K",
+		phone: "63804 95110",
+		post: "Treasurer",
 	},
 	{
-		personName: "Aadhitya",
-		email: "9884113766",
-	},
-	{
-		personName: "Kailash",
-		email: "9095541133",
-	},
-	{
-		personName: "Vignesh",
-		email: "8825708044",
-	},
-	{
-		personName: "Krishnan",
-		email: "6383551677",
-	},
-	{
-		personName: "Sneha Pillai",
-		email: "7358733362",
-	},
-	{
-		personName: "Aarthi Iyer",
-		email: "unavailable",
-	},
-	{
-		personName: "Aditya Kumar",
-		email: "9600194782",
-	},
-	{
-		personName: "Yashwanth",
-		email: "7010103495",
-	},
-	{
-		personName: "Vasudevakrishna",
-		email: "6383943553",
-	},
-	{
-		personName: "Thrishya",
-		email: "9384011184",
-	},
-	{
-		personName: "Sudharsana",
-		email: "9513787803",
-	},
-	{
-		personName: "Abhijit",
-		email: "9840406710",
-	},
-	{
-		personName: "Mohamed Fazith",
-		email: "9080527253",
-	},
-	{
-		personName: "Rozen Berg",
-		email: "unavailable",
-	},
-	{
-		personName: "Ezhil Mukhi",
-		email: "7305774686",
-	},
-	{
-		personName: "Monika",
-		email: "9840867891",
+		personName: "Vignesh S",
+		phone: "93602 65057",
+		post: "Member",
 	},
 ]
 
-let nonTechnical = [
-	{
-		personName: "Srinath",
-		email: "9789649388",
-	},
-	{
-		personName: "Shreeja",
-		email: "9150445617",
-	},
-	{
-		personName: "Logeshwaran",
-		email: "7339122647",
-	},
-	{
-		personName: "Riyaz Ahmed",
-		email: "9150567713",
-	},
-	{
-		personName: "Aravinth kumar",
-		email: "8870335281",
-	},
-	{
-		personName: "Vishnu",
-		email: "8939430854",
-	},
-	{
-		personName: "Sanjay Kannan",
-		email: "8428929784",
-	},
-	{
-		personName: "Srevardhani",
-		email: "9360120793",
-	},
-	{
-		personName: "Dafni Trisha",
-		email: "9443192988",
-	},
-	{
-		personName: "Geethanjali",
-		email: "6383051844",
-	},
-	{
-		personName: "Pooja",
-		email: "7395915055",
-	},
-	{
-		personName: "Gnanavarshini",
-		email: "6380614424",
-	},
-	{
-		personName: "Sowmya",
-		email: "8056493106",
-	},
-	{
-		personName: "Surya",
-		email: "9150986251",
-	},
-	{
-		personName: "Jagan",
-		email: "7540088216",
-	},
-	{
-		personName: "Veejendhiran P",
-		email: "6383647169",
-	}
-]
-
-export { technical, nonTechnical }
+export { members }
--- pages/Components/Contact/index.js
+++ pages/Components/Contact/index.js
@@ -1,6 +1,6 @@
 import styles from "./contact.module.css"
 import { v4 as UUID4 } from "uuid"
-import { technical, nonTechnical } from "./data"
+import { members } from "./data"
 
 export default function Home() {
 	return (
@@ -9,8 +9,7 @@ export default function Home() {
 			<div className={styles.contact}>
 				<span className={styles.heading}>Contact</span>
 				<div className={styles.contactContainer}>
-					<Technical technical={technical} />
-					<NonTechnical nonTechnical={nonTechnical} />
+					<Members members={members} />
 				</div>
 			</div>
 		</>
@@ -21,42 +20,23 @@ function Person(props) {
 	return (
 		<div className={styles.personItem}>
 			<span className={styles.personItemName}>{props.personName}</span>
-			<span className={styles.personItemEmail}>{props.email}</span>
+			<span className={styles.personItemEmail}>{props.phone}</span>
+			<span className={styles.personItemPost}>{props.post}</span>
 		</div>
 	)
 }
 
-function Technical(props) {
+function Members(props) {
 	return (
 		<div className={styles.contactItem}>
-			<span className={styles.contactItemHeading}>Technical Events</span>
 			<div className={styles.contactItemContent}>
-				{props.technical.map((item) => {
+				{props.members.map((item) => {
 					return (
 						<Person 
 							key={UUID4()}
 							personName={item.personName}
-							email={item.email}
-						/>
-					)
-				}
-				)}
-			</div>
-		</div>
-	)
-}
-
-function NonTechnical(props) {
-	return (
-		<div className={styles.contactItem}>
-			<span className={styles.contactItemHeading}>Non Technical Events</span>
-			<div className={styles.contactItemContent}>
-				{props.nonTechnical.map((item) => {
-					return (
-						<Person 
-							key={UUID4()}
-							personName={item.personName}
-							email={item.email}
+							phone={item.phone}
+							post={item.post}
 						/>
 					)
 				}
