3d9e8807416a13a2cd7f7d578d39ed53689e9d7f
tharunoptimus <tharunoptimus@outlook.com>
2022-12-09 05:33:09 +0530

    Added sponsors

--- pages/Components/Sponsors/index.js
+++ pages/Components/Sponsors/index.js
@@ -8,9 +8,56 @@ export default function Home() {
 				<span className={styles.heading}>Sponsors</span>
 
 				<div className={styles.sponsorsContainer}>
-
+					<Angris />
+					<PrivacyDefenders />
+					<MeowIt />
 				</div>
 			</div>
 		</>
+	)
+}
+
+function Angris() {
+	return (
+		<a href="https://angris.pages.dev" rel={"noopener noreferrer"} target={"_blank"}>
+			<img
+				src="./sponsors/angris.webp"
+				alt="Angris"
+				height={130}
+				width="auto"
+				aria-label="Angris"
+				title="Angris"
+			/>
+		</a>
+	)
+}
+
+function PrivacyDefenders() {
+	return (
+		<a href="https://privacydefenders.pages.dev" rel={"noopener noreferrer"} target={"_blank"}>
+			<img
+				src="./sponsors/privacydefenders.webp"
+				alt="Privacy Defenders"
+				height={130}
+				width="auto"
+				aria-label="Privacy Defenders"
+				title="Privacy Defenders"
+			/>
+		</a>
+	)
+}
+
+function MeowIt() {
+	return (
+		<a href="https://meowit.pages.dev" rel={"noopener noreferrer"} target={"_blank"}>
+			<img
+				src="./sponsors/meowit.webp"
+				alt="Meow It"
+				height={130}
+				width="auto"
+				aria-label="Meow It"
+				title="Meow It"
+			/>
+		</a>
 	)
 }
